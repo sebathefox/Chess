020632a4bbc13b44d008c31eeeba95bb5439bd5e
sebathefox <seba4928@edu.sde.dk>
2019-08-21 08:06:43 +0200

    Addeed gamestate

--- Game/Scripts/Network/GameState.cs
+++ Game/Scripts/Network/GameState.cs
@@ -1,7 +1,27 @@
+using System.Collections.Generic;
+
 namespace Game.Scripts.Network
 {
     public class GameState
     {
+        private GameColor _colorOfCurrentPlayer;
+
+        private List<Piece> _pieces;
+
+        public GameState(GameColor color, List<Piece> pieces)
+        {
+            _colorOfCurrentPlayer = color;
+            _pieces = pieces;
+        }
         
+        public GameColor ColorOfCurrentPlayer
+        {
+            get { return _colorOfCurrentPlayer; }
+        }
+
+        public List<Piece> Pieces
+        {
+            get { return _pieces; }
+        }
     }
 }
